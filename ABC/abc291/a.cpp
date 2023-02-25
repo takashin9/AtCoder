38617676793de02084c199e6195cae5009c785a1
takashin9 <91837495+takashin9@users.noreply.github.com>
2023-02-25 12:30:11 +0900

    create

---
+++ ABC/abc291/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+    #include <atcoder/all>
+using namespace atcoder;
+#endif
+
+using ll = long long;
+
+
+int main() {
+    
+    int n;
+    cin >> n;
+    cout << n << endl;
+    
+    return 0;
+}
