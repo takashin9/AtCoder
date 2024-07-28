e4ffc75a814090d9e628996913101fe97bb8543b
takashin9 <takashi64256@gmail.com>
2024-07-28 23:52:54 +0900

    WIP abc364 upsolve

--- ABC/abc364/abc364_d.cpp
+++ ABC/abc364/abc364_d.cpp
@@ -12,8 +12,24 @@ int main() {
     int n, q; cin >> n >> q;
     vector<int> a(n), b(q), k(q);
     for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < q; i++) cin >> b[i] >> k[i];
-    
+    sort(begin(a), end(a));
+
+    while (q--) {
+        int b, k; cin >> b >> k;
+        auto f = [&](int x) -> bool {
+            auto itr1 = lower_bound(begin(a), end(a), b - x);
+            auto itr2 = upper_bound(begin(a), end(a), b + x);
+            return itr2 - itr1 >= k;
+        };
+
+        int ng = -1, ok = 2e8;
+        while (abs(ng - ok) > 1) {
+            int mid = (ng + ok) / 2;
+            if (f(mid)) ok = mid;
+            else ng = mid;
+        }
+        cout << ok << endl;
+    }
     
     return 0;
 }
--- ABC/abc364/abc364_e.cpp
+++ ABC/abc364/abc364_e.cpp
@@ -14,18 +14,23 @@ int main() {
     for (int i = 0; i < n; i++) cin >> a[i];
     for (int i = 0; i < n; i++) cin >> b[i];
     
-    vector dp(n + 1, vector<vector<int>>(10001, vector<int>(10001, -1)));
-    for (int i = 0; i < n + 1; i++) {
-        dp[i][0][0] = 0;
-        dp[i][a[i]][b[i]] = 1;
-    }
+    vector dp(n + 1, vector(x + 1, vector<int>(n + 1, 1e9))); // dp[i][j][k]: i個目まで見て、甘さj、食べた料理の個数kのときのしょっぱさの最小値
+    dp[0][0][0] = 0;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 10001; j++) {
-            for (int k = 0; k < 10001; k++) {
-                dp[i + 1][j][k] = max(dp[i + 1][j][k], dp[i][j + a[i + 1]][k + b[i + 1]]);
+        for (int k = 0; k <= i; k++) {
+            for (int j = 0; j <= x; j++) {
+                dp[i + 1][j][k] = min(dp[i + 1][j][k], dp[i][j][k]);
+                if (j + a[i] <= x) dp[i + 1][j + a[i]][k + 1] = min(dp[i + 1][j + a[i]][k + 1], dp[i][j][k] + b[i]);
             }
         }
     }
+    int ans = 0;
+    for (int j = 0; j <= x; j++) {
+        for (int k = 0; k <= n; k++) {
+            if (dp[n][j][k] <= y) ans = max(ans, k);
+        }
+    }
+    cout << min(ans + 1, n) << endl;
 
     return 0;
 }
